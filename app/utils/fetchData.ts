eca241452bd7b07f90daa5bd49f5526e82ceafe7
Chris Barna <cbarna@theatlantic.com>
2021-12-21 12:24:14 -0700

    Update promise return type.

--- app/utils/fetchData.ts
+++ app/utils/fetchData.ts
@@ -1,6 +1,6 @@
 import { parse } from "csv-parse/sync";
 
-const fetchData = async (): WaitTime[] => {
+const fetchData = async (): Promise<WaitTime[]> => {
   const response = await fetch(
     "https://gist.githubusercontent.com/ctbarna/98b660129b01a5a2c050f3bab78aad70/raw/1d2291be64a304bf6fce7535b6fbbdf22b3079e5/wait.csv"
   );
