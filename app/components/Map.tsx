03774d7708b2111c30b8901cb647d98fef8b3d76
Chris Barna <cbarna@theatlantic.com>
2021-12-22 16:04:30 -0700

    Fixup type issues.

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -5,7 +5,7 @@ import { waitTimeMapping } from "../utils/fetchData";
 const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
-const formatPopup = (location: WaitTime) => (`
+const formatPopup = (location: WaitTime) => `
   <div class="font-sans px-2">
     <h3 class="text-lg font-bold py-2">Location</h3>
     <div class="text-md">${location.fullname}</div>
@@ -14,10 +14,10 @@ const formatPopup = (location: WaitTime) => (`
     <div class="text-md">Wait Time: ${location.wait_time}</div>
     <div class="text-md">Last Reported: ${location.last_reported}</div>
   </div>
-`);
+`;
 
 const Map = () => {
-  const [pageIsMounted, setPageIsMounted] = useState(false)
+  const [pageIsMounted, setPageIsMounted] = useState(false);
 
   const { selector } = useContext(LocationContext);
 
@@ -31,35 +31,47 @@ const Map = () => {
     );
   });
 
-  const colorScale = (time: number) => time === 6
-    ? "#ccc"
-    : scaleLinear()
-      .domain([0, 2, 3, 5]) // see `waitTimeMapping` in utils > fetchData.ts
-      .range(["#006837", "#e8db2a", "#f16e43", "#a50026"])(time)
+  const colorScale = (time: number) =>
+    time === 6
+      ? "#ccc"
+      : scaleLinear<string>()
+          .domain([0, 2, 3, 5]) // see `waitTimeMapping` in utils > fetchData.ts
+          .range(["#006837", "#e8db2a", "#f16e43", "#a50026"])(time);
 
   useEffect(() => {
     setPageIsMounted(true);
     const map = new mapboxgl.Map({
-      container: 'sites-map', // container ID
-      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      container: "sites-map", // container ID
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
       center: [-74.0183, 40.7077], // starting position [lng, lat]
-      zoom: 11 // starting zoom
+      zoom: 11, // starting zoom
     });
 
-    locations.forEach((loc): WaitTime => {
+    locations.forEach((loc: WaitTime) => {
       const { lat, lng, ordinal_wait_time } = loc;
       new mapboxgl.Marker({ color: colorScale(ordinal_wait_time) })
         .setLngLat({ lat, lng })
         .setPopup(new mapboxgl.Popup().setHTML(formatPopup(loc)))
-        .addTo(map)
-    })
-
-  })
+        .addTo(map);
+    });
+  }, [locations]);
 
   return (
-    <div id="sites-map" style={{ height: '75vh' }} >
-      <div className={`absolute z-10 top-2 left-2 bg-white p-2 rounded-sm ${pageIsMounted ? '' : 'hidden'}`}>
-        {Object.entries(waitTimeMapping).map(([key, time]) => <div key={key}><span style={{ backgroundColor: colorScale(time) }} className="inline-block w-3 h-3 mr-2" />{key}</div>)}
+    <div id="sites-map" style={{ height: "75vh" }}>
+      <div
+        className={`absolute z-10 top-2 left-2 bg-white p-2 rounded-sm ${
+          pageIsMounted ? "" : "hidden"
+        }`}
+      >
+        {Object.entries(waitTimeMapping).map(([key, time]) => (
+          <div key={key}>
+            <span
+              style={{ backgroundColor: colorScale(time) }}
+              className="inline-block w-3 h-3 mr-2"
+            />
+            {key}
+          </div>
+        ))}
       </div>
     </div>
   );
--- app/pages/map/index.tsx
+++ app/pages/map/index.tsx
@@ -2,7 +2,7 @@ import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import Header from "../../components/Header";
-import Map from "../../components/Map"
+import Map from "../../components/Map";
 import LocationStore from "../../components/LocationStore";
 import fetchData from "../../utils/fetchData";
 
@@ -15,15 +15,15 @@ const SiteMap: NextPage<Props> = ({ locations }) => {
     <LocationStore initialLocations={locations}>
       <Head>
         <title>Where are nyc testing sites?</title>
-        <meta
-          name="og:title"
-          content={`Where are nyc testing sites?`}
-        />
+        <meta name="og:title" content={`Where are nyc testing sites?`} />
         <meta
           name="og:description"
           content={`Map of NYC's public COVID testing sites.`}
         />
-        <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
       </Head>
       <Link href="/" passHref>
         <a className="underline text-blue-600 hover:text-blue-500 mb-2">Home</a>
@@ -32,8 +32,19 @@ const SiteMap: NextPage<Props> = ({ locations }) => {
         <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
           Map of City-Run Testing Locations
         </h1>
-        <p>NYC H&H Testing locations that include wait times. This list is not exhaustive; there are more locations listed without wait times. Check <a
-          className="underline text-blue-600 hover:text-blue-500" href="https://www.nychealthandhospitals.org/covid-19-testing-sites/" target="_blank">the NYC H&H testing sites webpage</a> for more details.</p>
+        <p>
+          NYC H&H Testing locations that include wait times. This list is not
+          exhaustive; there are more locations listed without wait times. Check{" "}
+          <a
+            className="underline text-blue-600 hover:text-blue-500"
+            href="https://www.nychealthandhospitals.org/covid-19-testing-sites/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            the NYC H&H testing sites webpage
+          </a>{" "}
+          for more details.
+        </p>
       </Header>
       <div className="mt-4 md:mt-8">
         <Map />
